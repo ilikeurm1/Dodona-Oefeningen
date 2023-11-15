6355762dade8ecb6b4b1f1660fe232c507147110
Matthijs Duhoux <ilikeurm1@gmail.com>
2023-11-15 15:53:06 +0100

    Added new excercise and redid ex3 and ex4

--- 4th Year/08. Doordenkertjes/3. Schaakspel - zelfde kleur.py
+++ 4th Year/08. Doordenkertjes/3. Schaakspel - zelfde kleur.py
@@ -1,22 +1,15 @@
-Black_x = [1,3,5,7]
-Black_y = [2,4,6,8]
-
 def IsWhite(x, y):
-    if x in Black_x:
-        if y in Black_y:
-            return True
-        return False  
+    if (x + y) % 2 == 1:
+        Color = "White"
     else:
-        if y in Black_y:
-            return False
-        return True
+        Color = "Black"
+    return Color
 
 x1 = int(input())
 y1 = int(input())
 
 x2 = int(input())
 y2 = int(input())
-
 
 Color_1 = IsWhite(x1, y1)
 Color_2 = IsWhite(x2, y2)
---
+++ 4th Year/08. Doordenkertjes/5. De verloren kaart.py
@@ -0,0 +1,36 @@
+entries = int(input())
+
+row = []
+full = []
+
+for x in range(entries + 1):
+    full.append(x + 1)
+
+for x in range(entries):
+    x = int(input())
+    row.append(x)
+
+for x in row:
+    full.remove(x)
+
+print(full[0])
+
+# Heb onderstaande code eerst gemaakt maar dan beseft dat code hierboven ook kon
+
+# entries = int(input())
+
+# row = []
+
+# for x in range(entries):
+#     x = int(input())
+#     row.append(x)
+
+# row.sort(reverse=True)
+
+# for x in row:
+#     y = x - 1
+#     if y == 0:
+#         y = entries + 1
+#     if y not in row:
+#         print(y)
+#         break

da3f43c4479060ea43d9a8d134eac79d41db0437
Matthijs Duhoux <ilikeurm1@gmail.com>
2024-04-30 10:41:15 +0200

    Fixed mistake in 3rd year...

--- 3rd Year/5. Keuze/4. Grootste van 3 getallen.py
+++ 3rd Year/5. Keuze/4. Grootste van 3 getallen.py
@@ -1,6 +1,6 @@
 print(f"{max(int(input()), int(input()), int(input()))} is het grootste getal")
 
-x = int(input())
-y = int(input())
-z = int(input())
-print(max(x, y, z))
+# x = int(input())
+# y = int(input())
+# z = int(input())
+# print(max(x, y, z))

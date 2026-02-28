bf601ef4584c08a76a12c5c51f35e0ef5f3a4c32
Matthijs <yeetloloofxd@gmail.com>
2026-02-28 11:01:26 +0100

    Small addition to exercise

--- 6th_Year/0_Introductions/lists/oef3.py
+++ 6th_Year/0_Introductions/lists/oef3.py
@@ -4,3 +4,4 @@ pows = [-5, 12, -3, 8, 0, -2, 15]
 
 print("Negative: [", *([n for n in pows if n > 0]), "]")
 print("Positive: [", *([n for n in pows if n < 0]), "]")
+print("Zeros (idx, 0): [", *([f"({i}, {n})" for i, n in enumerate(pows) if n == 0]), "]")
